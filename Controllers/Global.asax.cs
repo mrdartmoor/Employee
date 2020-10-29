0179bee3d263184aba24088d92afe4b443b18328
AzureAD\MarcinCieloch <dartmoor.lol@gmail.com>
2020-10-29 13:02:44 +0100

    prawie dzialajace resx

--- Controllers/Global.asax.cs
+++ Controllers/Global.asax.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Pracownicy.Controllers
@@ -10,20 +11,33 @@ namespace Pracownicy.Controllers
     {
         protected void Application_AcquireRequestState(object sender, EventArgs e)
 
-{
+        {
 
-   //Create culture info object 
+            //Create culture info object 
 
-   CultureInfo ci = new CultureInfo("pl");
+            CultureInfo ci = new CultureInfo("en-US");
 
 
 
-   System.Threading.Thread.CurrentThread.CurrentUICulture = ci;
+            //System.Threading.Thread.CurrentThread.CurrentUICulture = ci;
 
-  System.Threading.Thread.CurrentThread.CurrentCulture = 
+            //System.Threading.Thread.CurrentThread.CurrentCulture =
+            Thread.CurrentThread.CurrentCulture = ci;
+            Thread.CurrentThread.CurrentUICulture = ci;
 
-CultureInfo.CreateSpecificCulture(ci.Name);
+            //CultureInfo.CreateSpecificCulture(ci.Name);
 
- }
-}
+        }
+        //protected override void Application_AcquireRequestState()
+        //{
+        //    base.InitializeCulture();
+        //    System.Threading.Thread.CurrentThread.CurrentUICulture = new CultureInfo("fr-CA");
+        //    System.Threading.Thread.CurrentThread.CurrentCulture = new CultureInfo("fr-CA");
+        //}
+        //static void LanguageSet()
+        //{
+        //    Resourcess.Common.Culture = new CultureInfo("en-US");
+        //}
+        //LanguageSet();
+    }
 }
